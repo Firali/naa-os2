0f92e2c6e8578c61e55363e6f0f6beea6a7d4956
Firali <hasanova.firuze@bk.ru>
2022-05-30 23:46:25 +0400

    final

---
+++ Seminar/sem2/p3.py
@@ -0,0 +1,8 @@
+def checkDriveAge():
+            
+age = input('How old are you ?')
+  if int(age)<18:
+    print('Sorry you are not allowed to drive.Powering off. ')
+  elif int(age)>=18:
+    print('Hi enjoy the ride.Powering on! ');
+checkDriveAge()    
